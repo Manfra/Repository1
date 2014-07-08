cfcbcb0a9a2a69bd9b0b565a26f9f1ad7aaeeb1d
Mattia Manfredi <manfredimattia@gmail.com>
2014-07-08 09:55:08 +0200

    test

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+f = open('data.txt','r')
+localtime = time.asctime(time.localtime(time.time()))
+print  localtime   
+def Check(par1):
+    for line in f:
+        if line.upper().find(s.upper())!= -1:
+           par1=par1+1
+    if par1 == 0:
+        print 'No match found'
+    else:
+        print 'The word is found', par1 ,'times'
+
+s = raw_input('Enter the word you will found in the text: ')
+print 'Searching:', s
+Check(0)
+f.close()
+
+
+    
+    
+
