3cf049875da65ab9333d780cbf7041f40af6ea67
Manfra <manfredimattia@gmail.com>
2014-07-08 10:48:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def Check(par1):
         print 'No match found'
     else:
         print 'The word is found', par1 ,'times'
-
 s = raw_input('Enter the word you will found in the text: ')
 print 'Searching:', s
 Check(0)
