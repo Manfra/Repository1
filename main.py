f35d00ef501fe26122472ae790dd42585ba191a5
Manfra <manfredimattia@gmail.com>
2014-07-08 10:48:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
 f = open('data.txt','r')
-localtime = time.asctime(time.localtime(time.time()))
-print  localtime   
 def Check(par1):
     for line in f:
         if line.upper().find(s.upper())!= -1:
@@ -15,8 +12,3 @@ s = raw_input('Enter the word you will found in the text: ')
 print 'Searching:', s
 Check(0)
 f.close()
-
-
-    
-    
-
