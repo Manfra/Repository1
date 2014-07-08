ac4bddb519aee9e98f53f9d6e7416241998f4dc4
Manfra <manfredimattia@gmail.com>
2014-07-08 14:56:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 def Check():
-    n = 0
+    i = 0
     for line in f:
-        n = n + line.upper().count(s.upper())
-    print 'The word is found', n ,'times'
-
-while True:
-    try:
-        f = open(raw_input('Type the path of the .txt file:'))
-        print 'File successfully open'
-        break
-    except IOError:
-        print 'Error: File does not appear to exist.'
-        
-s = raw_input('Enter the word you will found in the text: ')
-print 'Searching:', s
-Check()
+        i += line.upper().count(s.upper())      
+    return i                                    
+try:
+    f = open(raw_input('Type the path of the .txt file:'))
+    print 'File successfully open'
+    s = raw_input('Enter the word you will found in the text: ')
+    print 'Searching:', s
+    count = Check()
+    print 'The word is found', count ,'times'
+except IOError:
+    print 'Error: File does not appear to exist.'
 f.close()
