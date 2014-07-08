148921f000426b55cb1f0a6f8e1c7dc7a8684eae
Manfra <manfredimattia@gmail.com>
2014-07-08 11:26:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-f = open('data.txt','r')
-def Check(par1):
+def Check(par):
     for line in f:
         if line.upper().find(s.upper())!= -1:
-           par1=par1+1
-    if par1 == 0:
+           par = par+1
+    if par == 0:
         print 'No match found'
     else:
-        print 'The word is found', par1 ,'times'
+        print 'The word is found', par ,'times'
+
+while True:
+    try:
+        f = open(raw_input('type the path of the .txt file:'))
+        print 'File successfully open'
+        break
+    except IOError:
+        print 'Error: File does not appear to exist.'
+        
 s = raw_input('Enter the word you will found in the text: ')
 print 'Searching:', s
 Check(0)
